e2be8fe93ee33a1aa160aa7e4914a6a02d82c388
jeevanrajsurya <jeevanrajsurya@gmail.com>
2026-03-11 16:45:39 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,30 +2,37 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const User = sequelize.define("User", {
+
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true
   },
+
   full_name: {
     type: DataTypes.STRING,
     allowNull: false
   },
+
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true
   },
+
   phone: {
     type: DataTypes.STRING
   },
+
   password: {
     type: DataTypes.STRING,
     allowNull: false
   },
+
   refreshToken: {
     type: DataTypes.TEXT
   }
+
 }, {
   tableName: "users",
   timestamps: false
